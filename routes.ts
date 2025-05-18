6efc5cda396327775e3fc385c272dfee9f512b75
Yolène <91735106+Yawleen@users.noreply.github.com>
2025-05-17 22:11:36 -0400

    Traduction des noms de route dans le fichier routes

--- routes.ts
+++ routes.ts
@@ -1,3 +1,3 @@
 export const HOME_ROUTE = "/";
-export const PRODUCTS_ROUTE = "/products";
-export const CHECKOUT_ROUTE = "/checkout";
+export const PRODUCTS_ROUTE = "/produits";
+export const CHECKOUT_ROUTE = "/panier";
